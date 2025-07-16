04b93a071e0c75656f64e3f6ac27fb6f237c2dc0
Rajkumar <rajkumar5068689@gmail.com>
2025-07-16 16:03:42 +0530

    new change

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,11 @@
 
 var _react = _interopRequireDefault(require("react"));
 var _client = _interopRequireDefault(require("react-dom/client"));
-var _App = _interopRequireDefault(require("./App"));
 var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
+var _RatingDemo = _interopRequireDefault(require("./Components/Pages/RatingDemo"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 const root = _client.default.createRoot(document.getElementById('root'));
-root.render(/*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_App.default, null)));
+root.render(/*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_RatingDemo.default, null)));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RatingDemo from './Components/Pages/RatingDemo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RatingDemo />
   </React.StrictMode>
 );
 
