df4bccf71e50087949493f16f35c00f49037f26f
Rajkumar <rajkumar5068689@gmail.com>
2025-07-16 19:17:36 +0530

    one more

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import RatingDemo from './Components/RatingDemo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RatingDemo />
   </React.StrictMode>
 );
